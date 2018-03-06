8c4290557a0bb319d60b0525458cd556abb1f4f0
YC <wuik2132@yahoo.com.tw>
2018-03-06 22:26:53 +0800

    2018/03/06新增類別TranslateExtension.cs

---
+++ EIPApp/EIPApp/Helpers/TranslateExtension.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Plugin.Multilingual;
+using System.Reflection;
+using System.Resources;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Threading;
+
+namespace EIPApp.Helpers
+{
+    [ContentProperty("Text")]
+    public class TranslateExtension : IMarkupExtension
+    {
+        const string ResourceId = "XFMultiLangual.Resources.AppResources";
+
+        static readonly Lazy<ResourceManager> resmgr = new Lazy<ResourceManager>(() => new ResourceManager(ResourceId, typeof(TranslateExtension).GetTypeInfo().Assembly));
+
+        public string Text { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Text == null)
+                return "";
+
+            var ci = Thread.CurrentThread.CurrentCulture;
+            var translation = resmgr.Value.GetString(Text, ci);
+
+            if (translation == null)
+            {
+
+#if DEBUG
+                throw new ArgumentException(
+                    String.Format("Key '{0}' was not found in resources '{1}' for culture '{2}'.", Text, ResourceId, ci.Name),
+                    "Text");
+#else
+                translation = Text; // returns the key, which GETS DISPLAYED TO THE USER
+#endif
+            }
+            return translation;
+        }
+    }
+}
